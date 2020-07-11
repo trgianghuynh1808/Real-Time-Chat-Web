10d05e6dc5ebeda3ea5252dda8baaad88d466f72
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-07-11 13:48:46 +0700

    update: header ui, and logout feature temp

--- front-end/src/components/Header/components/Main.jsx
+++ front-end/src/components/Header/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Main = ({ curDate }) => {
+const Main = ({ curDate, handleLogOut }) => {
   return (
     <div className="container-fluid">
       <div className="row justify-content-between align-items-center">
@@ -8,8 +8,13 @@ const Main = ({ curDate }) => {
           <i className="far fa-calendar-alt header__icon"></i>
           <span className="ml-3">{curDate}</span>
         </div>
-        <div className="col-1">
+        <div className="col-1 d-flex align-items-center">
           <i className="fas fa-bell header__icon float-right mr-3 cursor-pointer"></i>
+
+          <i
+            className="fas fa-sign-out-alt header__icon cursor-pointer ml-2"
+            onClick={handleLogOut}
+          ></i>
         </div>
       </div>
     </div>
--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import "./style.scss";
 import Main from "./components/Main";
 import { formatAMPM } from "utils";
 
 const Header = () => {
+  const history = useHistory();
+
+  const handleLogOut = () => {
+    history.push("/account/login");
+  };
+
   return (
     <div className="header">
-      <Main curDate={formatAMPM()} />
+      <Main curDate={formatAMPM()} handleLogOut={handleLogOut} />
     </div>
   );
 };
